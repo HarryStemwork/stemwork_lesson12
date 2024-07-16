3de6c1bb8e60f5166740edee62dc4fe65b7270bc
HarryStemwork <162728191+HarryStemwork@users.noreply.github.com>
2024-07-16 10:31:48 +0800

    Create GCD.py

---
+++ GCD.py
@@ -0,0 +1,6 @@
+def GCD(num1,num2):
+    #how to cal greatest common number
+    return gcd
+
+n1,n2 = eval(input("Please enter two numbers: "))
+print(f"The Greatest Common Number is {GCD(n1,n2)}")
